8e647b2d0db33ef32d024b169313c0e4c46d8e45
joaopelisson <joaopedro.devbr@gmail.com>
2023-04-26 21:27:16 -0300

    Component: SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionContainer, TransactionsTable } from "./styles";
 
 export function Transaction(){
@@ -8,6 +9,7 @@ export function Transaction(){
             <Header />
             <Summary />
             <TransactionContainer>
+                <SearchForm />
                 <TransactionsTable>
                     <tbody>
                         <tr>
